d131c80f73c9ebcc75264b08f5de19bfd42b54a7
Trista <tlr15@my.fsu.edu>
2017-03-03 21:24:26 +0000

    completed photo gallery

--- photos.js
+++ photos.js
@@ -16,14 +16,22 @@ var figureCount = 3;
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
-var filename;
-var currentFig;
 
-for (var i = 1; i < 4; i++) {
-filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
-currentFig = document.getElementsByTagName("img")[i - 1];
-currentFig.src = filename;
-}//end of for loop
+var filename;
+ var currentFig;
+ if (figureCount === 3) {
+    for (var i = 1; i < 4; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i - 1];
+        currentFig.src = filename;
+     }//end of for
+ } else {
+    for (var i = 0; i < 5; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i];
+        currentFig.src = filename;
+     }//end of for loop
+ }//end of else
 
 
 }//end of populateFigures function
@@ -74,20 +82,22 @@ function leftArrow() {
 /******************************************************************************************************/
 function previewFive() {
    
-// create figure and img elements for fifth image
+var articleEl = document.getElementsByTagName("article")[0];
+ // create figure and img elements for fifth image
  var lastFigure = document.createElement("figure");
+ 
  lastFigure.id = "fig5";
  lastFigure.style.zIndex = "5";
  lastFigure.style.position = "absolute";
  lastFigure.style.right = "45px";
  lastFigure.style.top = "67px";
-   
+ 
  var lastImage = document.createElement("img");
  lastImage.width = "240";
  lastImage.height = "135";
  
- //add or attach the fifth image to preview
  lastFigure.appendChild(lastImage);
+
  articleEl.appendChild(lastFigure);
  
  //clone figure element for fifth image and edit to be first image
@@ -108,7 +118,6 @@ function previewFive() {
  //disable the ‘Show more images’ button after it has been selected once
  var numberButton = document.querySelector("#fiveButton p");
  numberButton.removeEventListener("click", previewFive, false);
-
    
 }//end of previewFive()
 
